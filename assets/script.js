55c1e4b062cd34639813b72c74bc85902363e4e2
Atharva Pardeshi <atharva.exe@gmail.com>
2022-07-30 11:13:25 +0530

    declare variables

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("canvas1");
+const ctx = document.getContext("2d"); // 2 dimensional rendering context
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+let particleArray;
