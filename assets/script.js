0671006cc2df3494249c954367204f9f8a02cbaf
Atharva Pardeshi <atharva.exe@gmail.com>
2022-07-30 11:18:14 +0530

    feat: write Particle constructor function

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,13 @@ const ctx = document.getContext("2d"); // 2 dimensional rendering context
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 let particleArray;
+
+// create constructor function
+function Particle(x, y, directionX, directionY, size, color) {
+  this.x = x;
+  this.y = y;
+  this.directionX = directionX;
+  this.directionY = directionY;
+  this.size = size;
+  this.color = color;
+}
