50b8fb500ebdc83daa75f16da2086986ef30e74a
Atharva Pardeshi <atharva.exe@gmail.com>
2022-07-30 11:23:53 +0530

    feat: write draw method in the Particle prototype

--- assets/script.js
+++ assets/script.js
@@ -13,3 +13,19 @@ function Particle(x, y, directionX, directionY, size, color) {
   this.size = size;
   this.color = color;
 }
+
+// add draw method to particle prototype
+Particle.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+  ctx.fillStyle = this.color;
+  ctx.fill();
+};
+
+// writing the draw method in the prototype instead of the constructor itself
+// is considered a good practice. writing it in the constructor method would
+// create a draw method for each instance of the particle method, which will cause
+// performance issues
+
+const particle1 = new Particle(10, 10, 1, 1, 20, "white");
+particle1.draw();
